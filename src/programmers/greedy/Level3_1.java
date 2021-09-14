60b86e7d193818f1c400ab4f8a85cd3c3c5350e2
dexter <dex9205@gmail.com>
2021-09-14 15:18:52 +0900

    greed 진행중

--- src/programmers/greedy/Level3_1.java
+++ src/programmers/greedy/Level3_1.java
@@ -1,7 +1,15 @@
 package programmers.greedy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 // 섬 연결하기
 public class Level3_1 {
+    public static Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+    public static List<Integer> processed = new ArrayList<>();
+
     public static void main(String[] args) {
 //        n	costs	return
 //4	[[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]	4
@@ -14,6 +22,28 @@ public class Level3_1 {
         int answer = 0;
 
 
+        Map<Integer, Integer> weights = new HashMap<>();
+        Map<Integer, Integer> parents = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            graph.put(i, new HashMap<>());
+            weights.put(i, Integer.MAX_VALUE);
+            parents.put(i, null);
+        }
+        weights.remove(0);
+        parents.remove(0);
+
+        for(int i = 0; i < costs.length; i++){
+            graph.get(costs[i][0]).put(costs[i][1], costs[i][2]);
+            if(costs[i][0] == 0){
+                weights.put(costs[i][1], costs[i][2]);
+                parents.put(costs[i][1], 0);
+            }
+        }
+
+        System.out.println(graph.toString());
+        System.out.println(weights.toString());
+        System.out.println(parents.toString());
 
         return answer;
     }
