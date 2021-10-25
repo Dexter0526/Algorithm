52c9d5e5902c1c0a69e093f13d6347fd8e1ab103
dexter <dex9205@gmail.com>
2021-10-25 14:03:35 +0900

    1로 만들기

--- src/baekjoon/silver/Ex1463.java
+++ src/baekjoon/silver/Ex1463.java
@@ -1,23 +1,32 @@
 package baekjoon.silver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 // 1로 만들기
 public class Ex1463 {
+    public static Integer[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int depth = 0;
-        int n = scanner.nextInt();
+        int n = Integer.parseInt(bufferedReader.readLine());
+        dp = new Integer[n + 1];
+        dp[0] = 0;
+        dp[1] = 0;
 
-
+        System.out.println(dpRecursive(n));
     }
 
-    public static void bfs(int n, int depth){
-        depth++;
+    public static int dpRecursive(int n){
+        if(dp[n] == null){
+            if(n % 6 == 0) dp[n] = Math.min(dpRecursive(n / 3), Math.min(dpRecursive(n / 2), dpRecursive(n - 1))) + 1;
+            else if(n % 3 == 0) dp[n] = Math.min(dpRecursive(n / 3), dpRecursive(n - 1)) + 1;
+            else if(n % 2 == 0) dp[n] = Math.min(dpRecursive(n / 2), dpRecursive(n - 1)) + 1;
+            else dp[n] = dpRecursive(n - 1) + 1;
+        }
 
-        if(n == 1) return;
-
-
+        return dp[n];
     }
 }
