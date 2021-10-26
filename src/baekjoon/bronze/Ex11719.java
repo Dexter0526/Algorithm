e4a93f775fb656b02a0b7ced1ea877dffcc3f9b5
dexter <dex9205@gmail.com>
2021-10-26 14:47:16 +0900

    그대로 출력하기 2

---
+++ src/baekjoon/bronze/Ex11719.java
@@ -0,0 +1,17 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 그대로 출력하기 2
+public class Ex11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String read;
+
+        while ((read = bufferedReader.readLine()) != null){
+            System.out.println(read);
+        }
+    }
+}
