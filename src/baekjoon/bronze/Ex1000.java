46f4aaba675e936a8a67e8432dc78e53b3f59f8d
dexter <dex9205@gmail.com>
2021-12-13 19:29:59 +0900

    A+B

--- src/baekjoon/bronze/Ex1000.java
+++ src/baekjoon/bronze/Ex1000.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// A+B
 public class Ex1000 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
