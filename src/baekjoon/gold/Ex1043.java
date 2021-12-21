ff08254336345bf74ad530eafa391766c6b613c7
dexter <dex9205@gmail.com>
2021-12-21 21:54:09 +0900

    거짓말

--- src/baekjoon/gold/Ex1043.java
+++ src/baekjoon/gold/Ex1043.java
@@ -8,6 +8,8 @@ import java.util.StringTokenizer;
 
 // 거짓말
 public class Ex1043 {
+    public static int[] relationship;
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
@@ -23,10 +25,72 @@ public class Ex1043 {
             knownPeople.add(Integer.parseInt(stringTokenizer.nextToken()));
         }
 
+        relationship = new int[N + 1];
         HashSet<Integer>[] parties = new HashSet[M + 1];
+
+        for(int i = 1; i <= N; i++) {
+            relationship[i] = i;
+        }
+        for(int i = 1; i <= M; i++){
+            parties[i] = new HashSet<>();
+        }
+
         for(int i = 1; i <= M; i++){
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int number = Integer.parseInt(stringTokenizer.nextToken());
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+
+            parties[i].add(a);
+
+            while (number --> 1){
+                int b = Integer.parseInt(stringTokenizer.nextToken());
+                if(find(a) != find(b)) union(a, b);
+
+                parties[i].add(b);
+
+                a = b;
+            }
 
         }
+
+        HashSet<Integer> discovered = new HashSet<>();
+        for(int i = 1; i <= N; i++){
+            if(knownPeople.contains(i) && !discovered.contains(i)){
+                int root = find(i);
+                for(int j = 1; j <= N; j++){
+                    if (find(j) == root) {
+                        knownPeople.add(j);
+                        discovered.add(j);
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+        for(int i = 1; i <= M; i++){
+            boolean flag = false;
+            for(int person : parties[i]){
+                if(knownPeople.contains(person)){
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag) answer++;
+        }
+
+        System.out.println(answer);
+    }
+
+    private static int find(int people){
+        if(relationship[people] == people) return people;
+
+        relationship[people] = find(relationship[people]);
+        return relationship[people];
+    }
+
+    private static void union(int a, int b){
+        int relation = find(b);
+        relationship[relation] = a;
     }
 }
