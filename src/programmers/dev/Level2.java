366a786c5d90383fca29bfda66fa423ed6f3bc21
dexter <dex9205@gmail.com>
2021-12-28 15:04:14 +0900

    로또의 최고 순위와 최저 순위

--- src/programmers/dev/Level2.java
+++ src/programmers/dev/Level2.java
@@ -1,5 +1,6 @@
 package programmers.dev;
 
+// 로또의 최고 순위와 최저 순위
 public class Level2 {
     public static void main(String[] args) {
 //rows	columns	queries	result
@@ -13,18 +14,48 @@ public class Level2 {
     }
 
     public static int[] solution(int rows, int columns, int[][] queries) {
-        int[] answer = {};
-        int[][] matrix = new int[rows][columns];
-        int count = 0;
+        int[] answer = new int[queries.length];
+        int[][] matrix = new int[rows + 1][columns + 1];
+        int count = 1;
 
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < columns; j++){
-                count++;
-                matrix[i][j] = count;
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= columns; j++){
+                matrix[i][j] = count++;
             }
         }
 
+        for(int i = 0; i < queries.length; i++){
+            answer[i] = rotate(matrix, queries[i][0], queries[i][1], queries[i][2], queries[i][3]);
+        }
 
         return answer;
+    }
+
+    private static int rotate(int[][] matrix, int x1, int y1, int x2, int y2){
+        int init = matrix[x1][y1];
+        int min = init;
+
+        for(int i = x1; i < x2; i++){
+            matrix[i][y1] = matrix[i + 1][y1];
+            min = Math.min(min, matrix[i][y1]);
+        }
+
+        for (int i = y1; i < y2; i++) {
+            matrix[x2][i] = matrix[x2][i + 1];
+            min = Math.min(min, matrix[x2][i]);
+        }
+
+        for (int i = x2; i > x1; i--) {
+            matrix[i][y2] = matrix[i - 1][y2];
+            min = Math.min(min, matrix[i][y2]);
+        }
+
+        for (int i = y2; i > y1; i--) {
+            matrix[x1][i] = matrix[x1][i - 1];
+            min = Math.min(min, matrix[x1][i]);
+        }
+
+        matrix[x1][y1 + 1] = init;
+        return min;
     }
 }
