77aab7941d0e65d10033736a710ce46e4b252c2f
dexter <dex9205@gmail.com>
2022-01-11 18:19:32 +0900

    고양이

---
+++ src/baekjoon/bronze/Ex10171.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze;
+
+// 고양이
+public class Ex10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
