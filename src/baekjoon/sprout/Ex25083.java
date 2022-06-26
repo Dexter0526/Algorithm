58bffab33b125ed9bc856fd9959e6680079fa6b1
dexter <dex9205@gmail.com>
2022-06-26 13:20:27 +0900

    새싹

---
+++ src/baekjoon/sprout/Ex25083.java
@@ -0,0 +1,16 @@
+package baekjoon.sprout;
+
+// 새싹
+public class Ex25083 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("         ,r'\"7").append("\n");
+        stringBuilder.append("r`-_   ,'  ,/").append("\n");
+        stringBuilder.append(" \\. \". L_r'").append("\n");
+        stringBuilder.append("   `~\\/").append("\n");
+        stringBuilder.append("      |").append("\n");
+        stringBuilder.append("      |");
+
+        System.out.println(stringBuilder);
+    }
+}
