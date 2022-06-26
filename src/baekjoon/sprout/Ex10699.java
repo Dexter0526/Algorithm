527f63f219ba44e7ab7cb4db3abf98f1d4380aee
dexter <dex9205@gmail.com>
2022-06-26 13:13:58 +0900

    오늘 날짜

---
+++ src/baekjoon/sprout/Ex10699.java
@@ -0,0 +1,12 @@
+package baekjoon.sprout;
+
+import java.time.LocalDate;
+
+// 오늘 날짜
+public class Ex10699 {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+
+        System.out.println(localDate);
+    }
+}
